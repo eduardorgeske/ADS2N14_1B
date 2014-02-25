d778a72a0793b7b6989bbc53d1773c70d7ce8a53
eduardorgeske <eduardorg_22@hotmail.com>
2014-02-25 01:23:02 -0300

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package Primeira_Aula;
+import java.util.*;
+
+public class HelloWorld 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
+		
+		String username;
+		
+		System.out.println("Digite seu username:");
+		username=entrada.next();
+		
+		System.out.println("Hello "+username+"!");
+	}
+
+}
